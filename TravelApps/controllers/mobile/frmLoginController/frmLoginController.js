041efd09e889d0c765ba1742c85112f71697e4e2
najihah <najihah_rohmat@yahoo.com>
2018-01-11 22:25:05 +0800

    commit project

---
+++ TravelApps/controllers/mobile/frmLoginController/frmLoginController.js
@@ -0,0 +1,9 @@
+define({ 
+
+loginSuccess: function(response){
+       var navObj=new kony.mvc.Navigation("frmHome");
+        navObj.navigate();
+}       
+ });
+
+       
---
+++ TravelApps/controllers/mobile/frmLoginController/frmLoginControllerActions.js
@@ -0,0 +1,15 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_UWI_ee31dd06fe404516848f5c8ec46b98c1: function AS_UWI_ee31dd06fe404516848f5c8ec46b98c1(response) {
+        var self = this;
+        this.loginSuccessfunction(response);
+    },
+    AS_UWI_a9fde53dc95649518be6b046a408d915: function AS_UWI_a9fde53dc95649518be6b046a408d915() {
+        var self = this;
+    },
+    AS_UWI_b75eba8884624e448c949b094b1e3cae: function AS_UWI_b75eba8884624e448c949b094b1e3cae() {
+        var self = this;
+    }
+});
